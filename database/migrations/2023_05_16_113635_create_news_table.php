160260d2b3238da8d46a80a9a70e635675f3f297
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2023-05-17 15:49:53 +0500

    удалил ссылку на таблицу авторства

--- database/migrations/2023_05_16_113635_create_news_table.php
+++ database/migrations/2023_05_16_113635_create_news_table.php
@@ -15,10 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
-            $table->unsignedBigInteger('author_id');
             $table->timestamps();
-
-            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
